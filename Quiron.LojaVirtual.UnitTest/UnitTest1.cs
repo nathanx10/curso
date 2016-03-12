000cb3b48108916f3d35694e931e9dc59339bc99
nathanmasson <nathanmasson@hotmail.com>
2016-03-12 15:20:07 -0300

    aula 9

--- Quiron.LojaVirtual.UnitTest/UnitTest1.cs
+++ Quiron.LojaVirtual.UnitTest/UnitTest1.cs
@@ -1,5 +1,9 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Web.Mvc;
+using Quiron.LojaVirtual.Web.Models;
+using System.Linq;
+using Quiron.LojaVirtual.Web.HtmlHelpers;
 
 namespace Quiron.LojaVirtual.UnitTest
 {
@@ -9,6 +13,34 @@ namespace Quiron.LojaVirtual.UnitTest
         [TestMethod]
         public void TestMethod1()
         {
+        }
+
+        [TestMethod]
+        public void TestarSeAPaginacaoEstaSendoGeradaCorretamente()
+        {
+            //Arrange
+            HtmlHelper htmlHelper = null;
+            Paginacao paginacao = new Paginacao
+            {
+                PaginaAtual = 2,
+                ItensTotal = 28,
+                ItensPorPagina = 10
+            };
+
+            Func<int, string> paginaUrl = i => "Pagina" + i;
+
+
+            //Act
+            MvcHtmlString resultado = htmlHelper.PageLinks(paginacao, paginaUrl);
+
+            //Assert
+
+            Assert.AreEqual(
+                    @"<a class=""btn btn-default"" href=""Pagina1"">1</a>"
+                 + @"<a class=""btn btn-default btn-primary selected"" href=""Pagina2"">2</a>"
+                 + @"<a class=""btn btn-default"" href=""Pagina3"">3</a>", resultado.ToString()
+                 );
+
         }
     }
 }
