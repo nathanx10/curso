66c8d14bb9c69336a19a383f2c65cc2d1f8ce4a3
nathanmasson <nathanmasson@hotmail.com>
2016-03-13 14:35:59 -0300

    aula 16

--- Quiron.LojaVirtual.Dominio/Entidades/Carrinho.cs
+++ Quiron.LojaVirtual.Dominio/Entidades/Carrinho.cs
@@ -8,14 +8,14 @@ namespace Quiron.LojaVirtual.Dominio.Entidades
 {
     public class Carrinho
     {
-        private readonly List<ItemCarrinho> _itemCarrinho;
+        private readonly List<ItemCarrinho> _itemCarrinho = new List<ItemCarrinho>();
 
         //Adicionar
         public void AdicionarItem(Produto produto, int quantidade)
         {
             ItemCarrinho item = _itemCarrinho.FirstOrDefault(p => p.Produto.ProdutoId == produto.ProdutoId);
 
-            if(item == null)
+            if (item == null)
             {
                 _itemCarrinho.Add(new ItemCarrinho
                 {
@@ -23,20 +23,19 @@ namespace Quiron.LojaVirtual.Dominio.Entidades
                     Quantidade = quantidade
                 });
             }
-
             else
             {
                 item.Quantidade += quantidade;
             }
         }
 
-        //Remover
-        public void RemoverItem(Produto produto)
+        // Remover item
+        public void RemevorItem(Produto produto)
         {
-            _itemCarrinho.RemoveAll(l=>l.Produto.ProdutoId == produto.ProdutoId);
+            _itemCarrinho.RemoveAll(l => l.Produto.ProdutoId == produto.ProdutoId);
         }
 
-        //Obter valor total
+        //Obter o valor total
         public decimal ObterValorTotal()
         {
             return _itemCarrinho.Sum(e => e.Produto.Preco * e.Quantidade);
@@ -51,16 +50,15 @@ namespace Quiron.LojaVirtual.Dominio.Entidades
         //Itens carrinho
         public IEnumerable<ItemCarrinho> ItensCarrinho
         {
-            get
-            {
-                return _itemCarrinho;
-            }
+            get { return _itemCarrinho; }
         }
     }
+
 
     public class ItemCarrinho
     {
         public Produto Produto { get; set; }
+
         public int Quantidade { get; set; }
     }
 }
---
+++ Quiron.LojaVirtual.UnitTest/TesteCarrinhoCompras.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Quiron.LojaVirtual.Dominio.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiron.LojaVirtual.UnitTest
+{
+    [TestClass]
+    public class TesteCarrinhoCompras
+    {
+        [TestMethod]
+        public void AdicionarItensAoCarrinho()
+        {
+            //Arrange - criação dos produtos
+            Produto produto1 = new Produto
+            {
+                ProdutoId = 1,
+                Nome = "Teste1"
+            };
+
+            Produto produto2 = new Produto
+            {
+                ProdutoId = 2,
+                Nome = "Teste2"
+            };
+
+            Produto produto3 = new Produto
+            {
+                ProdutoId = 3,
+                Nome = "Teste3"
+            };
+
+            Carrinho carrinho = new Carrinho();
+           
+            carrinho.AdicionarItem(produto1, 2);
+            carrinho.AdicionarItem(produto2, 3);
+            carrinho.AdicionarItem(produto3, 3);
+
+
+            //Act
+            ItemCarrinho[] itens = carrinho.ItensCarrinho.ToArray();
+
+            //Assert
+            Assert.AreEqual(itens.Length, 3);
+            Assert.AreEqual(itens[0].Produto, produto1);
+            Assert.AreEqual(itens[1].Produto, produto2);
+
+
+
+        }
+
+    }
+}
