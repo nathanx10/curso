566e2a9279a3c8e2e692b6878caf103fb313b89c
nathanmasson <nathanmasson@hotmail.com>
2016-03-17 23:37:14 -0300

    aula 30 e 31

--- Quiron.LojaVirtual.Web/Areas/Administrativo/Controllers/ProdutoController.cs
+++ Quiron.LojaVirtual.Web/Areas/Administrativo/Controllers/ProdutoController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Quiron.LojaVirtual.Dominio.Entidades;
+using Quiron.LojaVirtual.Dominio.Repositorio;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +10,21 @@ namespace Quiron.LojaVirtual.Web.Areas.Administrativo.Controllers
 {
     public class ProdutoController : Controller
     {
-        // GET: Administrativo/Produto
+        ProdutosRepositorio _repositorio;
         public ActionResult Index()
         {
-            return View();
+            _repositorio = new ProdutosRepositorio();
+            var produtos = _repositorio.Produtos.ToList();
+            return View(produtos);
+        }
+
+        public ViewResult Alterar(int produtoId)
+        {
+            _repositorio = new  ProdutosRepositorio();
+            Produto produto = _repositorio.Produtos
+                .FirstOrDefault(p=>p.ProdutoId == produtoId);
+
+            return View(produto);
         }
     }
 }
