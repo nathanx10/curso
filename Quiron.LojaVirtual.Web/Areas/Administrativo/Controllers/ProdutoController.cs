d68d125fa7fb4c1dfa70b97bbb591188941db910
nathanmasson <nathanmasson@hotmail.com>
2016-03-19 21:55:36 -0300

    aula 35 36 37

--- Quiron.LojaVirtual.Web/Areas/Administrativo/Controllers/ProdutoController.cs
+++ Quiron.LojaVirtual.Web/Areas/Administrativo/Controllers/ProdutoController.cs
@@ -10,29 +10,31 @@ namespace Quiron.LojaVirtual.Web.Areas.Administrativo.Controllers
 {
     public class ProdutoController : Controller
     {
-        ProdutosRepositorio _repositorio;
+        private ProdutosRepositorio _repositorio;
+
         public ActionResult Index()
         {
             _repositorio = new ProdutosRepositorio();
-            var produtos = _repositorio.Produtos.ToList();
+            var produtos = _repositorio.Produtos;
             return View(produtos);
         }
 
+
         public ViewResult Alterar(int produtoId)
         {
-            _repositorio = new  ProdutosRepositorio();
+            _repositorio = new ProdutosRepositorio();
             Produto produto = _repositorio.Produtos
-                .FirstOrDefault(p=>p.ProdutoId == produtoId);
-
-            TempData["acao"] = "Alterar produto";
+                .FirstOrDefault(p => p.ProdutoId == produtoId);
 
             return View(produto);
         }
 
+
+
         [HttpPost]
         public ActionResult Alterar(Produto produto)
         {
-            if(ModelState.IsValid)
+            if (ModelState.IsValid)
             {
                 _repositorio = new ProdutosRepositorio();
                 _repositorio.Salvar(produto);
@@ -40,54 +42,47 @@ namespace Quiron.LojaVirtual.Web.Areas.Administrativo.Controllers
                 TempData["mensagem"] = string.Format("{0} foi salvo com sucesso", produto.Nome);
 
                 return RedirectToAction("Index");
+
             }
             return View(produto);
         }
 
         public ViewResult NovoProduto()
         {
-            TempData["acao"] = "Adicionar novo produto";
             return View("Alterar", new Produto());
         }
 
-        [HttpPost]
-        public ActionResult NovoProduto(Produto produto)
-        {
-            if (ModelState.IsValid)
-            {
-                _repositorio = new ProdutosRepositorio();
-                _repositorio.Salvar(produto);
-
-                TempData["mensagem"] = string.Format("{0} foi adicionado com sucesso", produto.Nome);
-            }
-
-            return RedirectToAction("Index");
-        }
 
         //[HttpPost]
         //public ActionResult Excluir(int produtoId)
         //{
         //    _repositorio = new ProdutosRepositorio();
+
         //    Produto prod = _repositorio.Excluir(produtoId);
 
-        //    if(prod != null)
+        //    if (prod != null)
         //    {
-        //        TempData["mensagem"] = string.Format("{0} foi excluido com sucesso", prod.Nome);
+        //        TempData["mensagem"] = string.Format("{0} excluído com sucesso", prod.Nome);
         //    }
 
         //    return RedirectToAction("Index");
         //}
 
+
+
         [HttpPost]
         public JsonResult Excluir(int produtoId)
         {
+            // System.Threading.Thread.Sleep(TimeSpan.FromSeconds(5));
+
             string mensagem = string.Empty;
             _repositorio = new ProdutosRepositorio();
+
             Produto prod = _repositorio.Excluir(produtoId);
 
             if (prod != null)
             {
-                mensagem = string.Format("{0} foi excluido com sucesso", prod.Nome);
+                mensagem = string.Format("{0} excluído com sucesso", prod.Nome);
             }
 
             return Json(mensagem, JsonRequestBehavior.AllowGet);
