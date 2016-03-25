3d6b2edeb8b94151f4423ee16106a595dfe7b24d
nathanmasson <nathanmasson@hotmail.com>
2016-03-25 15:25:49 -0300

    aula 55

---
+++ Quiron.LojaVirtual.Dominio/Repositorio/CategoriaRepositorio.cs
@@ -0,0 +1,19 @@
+﻿using Quiron.LojaVirtual.Dominio.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiron.LojaVirtual.Dominio.Repositorio
+{
+    public class CategoriaRepositorio
+    {
+        private readonly EfDbContext _context = new EfDbContext();
+
+        public IEnumerable<Categoria> ObterCategorias()
+        {
+            return _context.Categorias.OrderBy(c => c.CategoriaDescricao);
+        }
+    }
+}
